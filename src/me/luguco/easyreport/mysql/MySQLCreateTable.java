ed8e98e88d9b8cb710f3614b9ff1a49e72ff0b06
luguco <syant@web.de>
2017-10-06 18:39:01 +0200

    Simplify table creation

--- src/me/luguco/easyreport/mysql/MySQLCreateTable.java
+++ src/me/luguco/easyreport/mysql/MySQLCreateTable.java
@@ -8,64 +8,56 @@ import java.sql.SQLException;
  */
 public class MySQLCreateTable {
 
-    public static void CreateTables(){
-        if(MySQLConnection.isConnected()){
+    public static void CreateTables() {
+        if (MySQLConnection.isConnected()) {
 
             PreparedStatement ps;
 
             try {
-                ps = MySQLConnection.getConnection().prepareStatement("SELECT * FROM `ReportedPlayer`");
-                ps.executeQuery();
-            } catch (SQLException e) {
-
-                try {
-                    ps = MySQLConnection.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `ReportedPlayer` (`Player` VARCHAR(100),`UUID` VARCHAR (100),`Reason` VARCHAR(100),`Proof` VARCHAR(100),`Reporter` VARCHAR(100),`Date` TIMESTAMP default CURRENT_TIMESTAMP,`Report_Claimed` VARCHAR(100),`Supporter` VARCHAR(100))");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE ReportedPlayer AUTO_INCREMENT = 1;");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE ReportedPlayer ADD `Count` INT PRIMARY KEY AUTO_INCREMENT FIRST");
-                    ps.executeUpdate();
-
-                    ps = MySQLConnection.getConnection().prepareStatement("INSERT INTO ReportedPlayer (`Player`,`UUID`,`Reason`,`Proof`,`Reporter`,`Report_Claimed`,`Supporter`) VALUES ('Testplayer','TestUUID','Hacking','www.testproof.de','Testreporter','false','NULL')");
-                    ps.executeUpdate();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+                ps = MySQLConnection.getConnection().prepareStatement(
+                        "CREATE TABLE IF NOT EXISTS `ReportedPlayer` (\n" +
+                                "  `Count` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                                "  `Player` varchar(100)  DEFAULT NULL,\n" +
+                                "  `UUID` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Reason` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Proof` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Reporter` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,\n" +
+                                "  `Report_Seen` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Supporter` varchar(100)  DEFAULT NULL,\n" +
+                                "  PRIMARY KEY (`Count`)\n" +
+                                ")");
+                ps.executeUpdate();
+                ps = MySQLConnection.getConnection().prepareStatement("INSERT INTO ReportedPlayer (`Player`,`UUID`,`Reason`,`Proof`,`Reporter`,`Report_Claimed`,`Supporter`) VALUES ('Testplayer','TestUUID','Hacking','www.testproof.de','Testreporter','false','NULL')");
+                ps.executeUpdate();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
 
             try {
-                ps = MySQLConnection.getConnection().prepareStatement(" SELECT * FROM `ReportCount`");
-                ps.executeQuery();
-            } catch (SQLException e) {
-                try {
-                    ps = MySQLConnection.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `ReportCount` (`Player` VARCHAR(100),`UUID` VARCHAR(100),`Counter` INT(100))");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ReportCount` AUTO_INCREMENT = 1;");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ReportCount` ADD `Count` INT PRIMARY KEY AUTO_INCREMENT FIRST");
-                    ps.executeUpdate();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+                ps = MySQLConnection.getConnection().prepareStatement(
+                        "CREATE TABLE IF NOT EXISTS `ReportCount` (\n" +
+                                "  `Count` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                                "  `Player` varchar(100)  DEFAULT NULL,\n" +
+                                "  `UUID` varchar(100)  DEFAULT NULL,\n" +
+                                "  `Counter` int(100) DEFAULT NULL,\n" +
+                                "  PRIMARY KEY (`Count`)\n" +
+                                ")");
+                ps.executeUpdate();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
 
             try {
-                ps = MySQLConnection.getConnection().prepareStatement(" SELECT * FROM `ClosedReports`");
-                ps.executeQuery();
-            } catch (SQLException e) {
-
-                try {
-                    ps = MySQLConnection.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `ClosedReports` (`Player` VARCHAR(100),`UUID` VARCHAR (100),`Reason` VARCHAR(100),`Proof` VARCHAR(100),`Reporter` VARCHAR(100),`Date` DATETIME,`CloseDate` TIMESTAMP default CURRENT_TIMESTAMP,`Closer` VARCHAR(100),`Result`VARCHAR(100))");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ClosedReports` AUTO_INCREMENT = 1;");
-                    ps.executeUpdate();
-                    ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ClosedReports` ADD `Count` INT PRIMARY KEY AUTO_INCREMENT FIRST");
-                    ps.executeUpdate();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+                ps = MySQLConnection.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `ClosedReports` (`Player` VARCHAR(100),`UUID` VARCHAR (100),`Reason` VARCHAR(100),`Proof` VARCHAR(100),`Reporter` VARCHAR(100),`Date` DATETIME,`CloseDate` TIMESTAMP default CURRENT_TIMESTAMP,`Closer` VARCHAR(100),`Result`VARCHAR(100))");
+                ps.executeUpdate();
+                ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ClosedReports` AUTO_INCREMENT = 1;");
+                ps.executeUpdate();
+                ps = MySQLConnection.getConnection().prepareStatement("ALTER TABLE `ClosedReports` ADD `Count` INT PRIMARY KEY AUTO_INCREMENT FIRST");
+                ps.executeUpdate();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
-            //sdga
         }
     }
 }
